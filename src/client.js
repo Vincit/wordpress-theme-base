d2c487b642f5f135857aebabf382653c613469ac
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-08-24 17:11:30 +0300

    Add explanation

--- src/client.js
+++ src/client.js
@@ -8,6 +8,10 @@ if (module.hot) {
   module.hot.accept('./js/components/clock', () => {
     const nextClock = require('./js/components/clock').default();
 
+    // I know what you're thinking. "Why not just call component() again?"
+    // Because it has not changed. We have to require() it again to get the fresh one.
+    // It's also possible to use dynamic imports instead of require().
+
     document.body.replaceChild(nextClock, clockEl);
     clockEl = nextClock;
   });
