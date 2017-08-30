75d3c4d32c500958fc1137a7e89b7c01a2587d10
Christian Nikkanen <me@kisu.li>
2017-08-30 11:37:55 +0300

    Add note

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
 
-/* Instead of polluting this file with thousands of lines, add your module into inc/ and it is "autoloaded". */
+/* 
+ * Instead of polluting this file with thousands of lines, add your module into inc/ and it is "autoloaded
+ * Do not output anything in these files directly! Output is shown in REST responses and displayed on the page.
+ */
 foreach (glob(dirname(__FILE__) . "/inc/*") as $filename) {
   require_once($filename);
 }
