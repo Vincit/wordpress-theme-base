1bb94baafcf9a9ebb0e7e94ec33b207bb4f1629c
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-09-22 18:37:09 +0300

    Declare theme support

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -1,6 +1,22 @@
 <?php
 namespace Vincit;
 
-add_theme_support("custom-logo");
-add_theme_support("post-thumbnails");
-add_theme_support("title-tag");
+add_action("after_setup_theme", function() {
+  add_theme_support("custom-logo");
+  add_theme_support("post-thumbnails");
+  add_theme_support("title-tag");
+  add_theme_support("html", [
+    "search-form",
+    "comment-list",
+    "gallery",
+    "caption",
+    // "comment-form" // until someone makes the hard coded novalidate attr filterable this stays off.
+  ]);
+
+  $GLOBALS["content_width"] = 800; // Width of the actual content in single page.
+  add_theme_support("soil-clean-up");
+  add_theme_support("soil-disable-trackbacks");
+  add_theme_support("soil-nav-walker");
+  add_theme_support("soil-nice-search");
+  add_theme_support("soil-disable-asset-versioning");
+});
