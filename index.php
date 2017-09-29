660d995649aefb357403208b78cb78c603f895c0
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-09-29 16:24:31 +0300

    List posts on frontpage

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
-<div class="container">
 
+<div class="container">
+  <?php while (have_posts()) { the_post(); ?>
+  <article <?php post_class()?>>
+    <a href="<?=get_permalink()?>">
+      <?=the_title()?>
+    </a>
+  </article>
+  <?php } ?>
 </div>
+
 <?php get_footer();
