d56e7fcc1dc4b332ed3013f1c7db4a46054a9a97
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-09-29 15:22:14 +0300

    Require files only

--- functions.php
+++ functions.php
@@ -1,9 +1,11 @@
 <?php
 
-/* 
+/*
  * Instead of polluting this file with thousands of lines, add your module into inc/ and it is "autoloaded
- * Do not output anything in these files directly! Output is shown in REST responses and displayed on the page.
+ * Do not output anything in these files directly! Output is shown in REST responses and displayed on the page, and it breaks RSS feeds.
  */
 foreach (glob(dirname(__FILE__) . "/inc/*") as $filename) {
-  require_once($filename);
+  if (!is_dir($filename)) {
+    require_once($filename);
+  }
 }
