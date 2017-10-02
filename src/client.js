9aa6dc8b3a9edbbac1752f476b74cfe880285c1e
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-10-02 13:03:03 +0300

    Move to conform to linting rules

--- src/client.js
+++ src/client.js
@@ -1,16 +1,27 @@
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import 'normalize.css';
-import './client.styl';
-import './js/skip-link-focus-fix';
+
 import MobileNavigation from './js/components/MobileNavigation';
 import EnhanceWPElements from './js/components/EnhanceWPElements';
 import showSampleWidgets from './js/sample';
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+
+import './client.styl';
+import './js/skip-link-focus-fix';
 
 new MobileNavigation();
 new EnhanceWPElements(['body .pagebuilder', 'article']);
 
 showSampleWidgets(document.querySelector('.site-footer .container'));
 OfflinePluginRuntime.install();
+
+const o1 = { a: 'hello' };
+const o2 = { b: 'world' };
+
+// console.log(Object.assign({}, o1, o2));
+console.log({
+  ...o1,
+  ...o2,
+});
 
 if (module.hot) {
   // Running from webpack-dev-server. Transform all links so they keep working.
