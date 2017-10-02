b5c3075964c775ccad172a0d2ef30354c6420a89
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-10-02 11:56:49 +0300

    Add some article styles

--- inc/templates/SingleArticle.php
+++ inc/templates/SingleArticle.php
@@ -2,15 +2,24 @@
 namespace Vincit\template;
 
 function SingleArticle($data = []) { ?>
-  <article <?php post_class()?>>
-    <header>
+  <article <?php post_class("single-article")?>>
+    <header class="article-header">
       <h1><?=$data["title"]?></h1>
     </header>
-    <section>
+
+    <section class="article-content">
       <?=$data["content"]?>
     </section>
-    <footer>
 
+    <footer class="article-footer">
+    <?php
+    global $numpages; /* If post has pagebreaks, and the post is shown in singular form,
+    show the pagination. */
+    if (is_singular() && $numpages > 1) { ?>
+      <div class="pagination article-pagination">
+        <?php wp_link_pages(); ?>
+      </div>
+    <?php } ?>
     </footer>
   </article>
 <?php }
