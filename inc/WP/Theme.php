cdbb48ec4c01e4b960992d1a7ade6b975f051d3a
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-10-10 15:42:00 +0300

    Configure Relevanssi

--- inc/WP/Theme.php
+++ inc/WP/Theme.php
@@ -20,3 +20,70 @@ add_action("after_setup_theme", function () {
   add_theme_support("soil-nice-search");
   add_theme_support("soil-disable-asset-versioning");
 });
+
+function configure_relevanssi($force = false) {
+  if ($force || !get_option("vincit_relevanssi_configured")) {
+    /* Legend
+       *: Comma separated list of IDs.
+    */
+    $opts = [
+      "title_boost" => 5,
+      "comment_boost" => 0.75,
+      "admin_search" => "on",
+      "highlight" => "class",
+      "txt_col" => "#ff0000",
+      "bg_col" => "#ffaf75",
+      "css" => "text-decoration: underline; text-color: #ff0000",
+      "class" => "relevanssi-query-term",
+      "excerpts" => "on",
+      "excerpt_length" => 30,
+      "excerpt_type words" => "",
+      "excerpt_allowable_tags" => "<p><a><strong><em>",
+      "log_queries" => "off",
+      "log_queries_with_ip" => "off",
+      "cat" => 0, // *
+      "excat" => 0, // *
+      "extag" => 0, // *
+      "index_fields" => "visible",
+      "exclude_posts" => "",
+      "hilite_title" => "off",
+      "highlight_docs" => "off",
+      "highlight_comments" => "off",
+      "index_comments" => "none",
+      "show_matches" => "on",
+      "show_matches_text" => "<!--
+Search hits:
+  %body% in body,
+  %title% in title,
+  %categories% in categories,
+  %tags% in tags,
+  %taxonomies% in other taxonomies,
+  %comments% in comments.
+Score: %score%
+-->
+",
+      "fuzzy" => "sometimes",
+      "expand_shortcodes" => "on",
+      "index_author" => "off",
+      "implicit_operator" => "AND",
+      "omit_from_logs" => "",
+      "synonyms" => "",
+      "index_excerpt" => "off",
+      "index_limit" => 500,
+      "disable_or_fallback" => "off",
+      "respect_exclude" => "on",
+      "min_word_length" => 3,
+      "wpml_only_current" => "on",
+      "word_boundaries" => "on",
+      "default_orderby" => "relevance",
+      "throttle" => "on",
+      "throttle_limit" => 500,
+    ];
+
+    foreach ($opts as $key => $value) {
+      \update_option("relevanssi_$key", $value);
+    }
+
+    \update_option("vincit_relevanssi_configured", time());
+  }
+} configure_relevanssi(true);
