2b0881864fcc61c23e54b1def7491f387387c176
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-10-11 10:42:44 +0300

    Fix radio buttons

--- src/js/lib/form.js
+++ src/js/lib/form.js
@@ -1,40 +1,52 @@
 import { el } from 'redom';
 
-const update = (mock, actual) => {
-  if (actual.checked) {
-    mock.classList.add('checked');
-    mock.setAttribute('aria-checked', true);
-  } else {
-    mock.classList.remove('checked');
-    mock.setAttribute('aria-checked', false);
-  }
-};
-
-const mockbox = (actualCheckbox) => {
+const mockbox = (real) => {
   const checkbox = el('.mockbox', {
     role: 'checkbox',
     'aria-checked': false,
   });
 
-  actualCheckbox.style.position = 'absolute';
-  actualCheckbox.style.left = '-9001px';
-  actualCheckbox.addEventListener('change', update.bind(null, checkbox, actualCheckbox));
-  actualCheckbox.parentNode.insertBefore(checkbox, actualCheckbox);
+  real.style.position = 'absolute';
+  real.style.left = '-9001px';
+  real.parentNode.insertBefore(checkbox, real);
+  real.addEventListener('change', () => {
+    if (real.checked) {
+      checkbox.classList.add('checked');
+      checkbox.setAttribute('aria-checked', true);
+    } else {
+      checkbox.classList.remove('checked');
+      checkbox.setAttribute('aria-checked', false);
+    }
+  });
 
   return checkbox;
 };
 
-const mockradio = (actualRadio) => {
+const mockradio = (real) => {
   const radio = el('.mockradio', {
     role: 'radio',
     'aria-checked': false,
   });
+  const form = real.closest('form');
 
-  console.log(actualRadio);
-  // actualRadio.style.position = 'absolute';
-  actualRadio.style.left = '-9001px';
-  actualRadio.addEventListener('change', update.bind(null, radio, actualRadio));
-  actualRadio.parentNode.insertBefore(radio, actualRadio);
+  real.style.position = 'absolute';
+  real.style.left = '-9001px';
+  real.parentNode.insertBefore(radio, real);
+  real.addEventListener('change', () => {
+    // It's necessary to query on every change, as more elements may have appeared.
+    const members = Array.from(form.querySelectorAll(`[name="${real.name}"]`));
+
+    members.forEach((member) => {
+      const mock = member.previousElementSibling;
+      if (real.checked && member === real) {
+        mock.classList.add('checked');
+        mock.setAttribute('aria-checked', true);
+      } else {
+        mock.classList.remove('checked');
+        mock.setAttribute('aria-checked', false);
+      }
+    });
+  });
 
   return radio;
 };
