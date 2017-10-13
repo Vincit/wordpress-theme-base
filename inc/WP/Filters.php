d860570a0edf024d2db14fe91264cc466eecc0fd
Christian Nikkanen <me@kisu.li>
2017-10-13 10:43:34 +0300

    Prevent Gravity Forms from destroying perf

--- inc/WP/Filters.php
+++ inc/WP/Filters.php
@@ -47,3 +47,17 @@ function strip_empty_paragraphs($content) {
 }
 
 add_filter("the_content", "\\Vincit\\WP\\Filters\\strip_empty_paragraphs");
+
+
+// Gravity Forms makes some absolutely mental decisions. 
+// Loading scripts in head? Not on my watch.
+add_filter("gform_tabindex", "\\__return_false");
+add_filter("gform_init_scripts_footer", "\\__return_true");
+
+add_filter("gform_cdata_open", function () {
+  return "document.addEventListener('DOMContentLoaded', function() { ";
+});
+
+add_filter("gform_cdata_close", function () {
+  return "}, false);";
+});
