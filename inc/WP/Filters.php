4e6c33e85ff238f6fc97ae643e54b7614ae96998
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-10-17 16:05:19 +0300

    Add stages to menus

--- inc/WP/Filters.php
+++ inc/WP/Filters.php
@@ -68,3 +68,19 @@ add_filter("gform_cdata_close", function () {
 
 // Disable "traffic lights"
 add_filter("the_seo_framework_show_seo_column", "__return_false");
+
+/*
+ * Add stages to menus so if it's required for something it's there.
+ */
+$menu_lookup = [];
+add_filter("nav_menu_css_class", function ($classes, $item) use (&$menu_lookup) {
+  $menu_lookup[$item->ID] = [
+    "parent" => $item->menu_item_parent,
+    "level" => $item->menu_item_parent !== '0' // Why is it a string?
+      ? $menu_lookup[(int) $item->menu_item_parent]["level"] + 1
+      : 0
+  ];
+
+  $classes[] = "level-{$menu_lookup[$item->ID]["level"]}";
+  return $classes;
+}, 999999, 2); // We want that the class is the last one.
