2a410e492f11fc1766c13a659b8adfe6be585426
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-10-20 16:18:24 +0300

    Remove dead code

--- src/js/wpQuery.js
+++ src/js/wpQuery.js
@@ -9,28 +9,12 @@ async function wpQuery(queryArgs = false) {
   };
 
   // Got an error about no such route? https://github.com/aucor/wp_query-route-to-rest-api
-  const response = await req(`/wp-json/AAAAA/wp_query/args/?${qs.stringify(args)}`);
+  const response = await req(`/wp-json/wp_query/args/?${qs.stringify(args)}`);
   const headers = Array.from(response.headers.entries()).reduce((acc, a) => {
     acc[a[0]] = a[1];
     return acc;
   }, {});
   const posts = await response.json();
-  const statusCode = response.status;
-  const stringStatusCode = String(statusCode);
-
-  if (stringStatusCode[0] !== '2') {
-    if (statusCode === 404) {
-      throw new Error(error(
-        statusCode,
-        'Got 404 trying to query for posts. Is aucor/wp_query-route-to-rest-api installed?'
-      ));
-    }
-
-    console.warn(`Response status was ${response.status}`);
-    if (stringStatusCode[0] === '4' || stringStatusCode[0] === '5') {
-      throw new Error(error(statusCode, response.statusText));
-    }
-  }
 
   return { headers, posts };
 }
