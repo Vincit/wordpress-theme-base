784f5e70fb7c5ad185b9b1b61a1bc6513c535287
Christian Nikkanen <me@kisu.li>
2017-11-15 15:07:35 +0200

    Disable link transform in admin, redirect instead

--- src/admin.js
+++ src/admin.js
@@ -3,5 +3,12 @@ import transformURLsWebpackDevServer from './js/lib/transform-urls-wds';
 import './admin.styl';
 
 if (module.hot) {
-  transformURLsWebpackDevServer();
+  if (window.location.origin.includes('localhost')) {
+    window.location.href = window.location.href.replace(
+      window.location.origin,
+      window.theme.siteurl
+    );
+  }
+
+  // transformURLsWebpackDevServer();
 }
