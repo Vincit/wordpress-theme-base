f6fc5ff6c50d2f9eb48f2f3884e5535f2a473d2e
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-11-16 12:57:05 +0200

    Allow login from localhost to show admin bar on front

--- src/admin.js
+++ src/admin.js
@@ -1,12 +1,15 @@
+import transformURLsWebpackDevServer from './js/lib/transform-urls-wds';
 import './admin.styl';
 
 if (module.hot) {
-  if (window.location.origin.includes('localhost')) {
-    window.location.href = window.location.href.replace(
-      window.location.origin,
-      window.theme.siteurl
-    );
+  if (document.body.classList.contains('login')) {
+    transformURLsWebpackDevServer();
+  } else {
+    if (window.location.origin.includes('localhost')) { // eslint-disable-line
+      window.location.href = window.location.href.replace(
+        window.location.origin,
+        window.theme.siteurl
+      );
+    }
   }
-
-  // transformURLsWebpackDevServer();
 }
