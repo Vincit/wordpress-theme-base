e77577f1d0824e6811d7b72e6bece63973ed89c1
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-12-19 14:51:13 +0200

    Refactor AlternatingBlock

--- inc/templates/AlternatingBlock.php
+++ inc/templates/AlternatingBlock.php
@@ -2,27 +2,57 @@
 namespace Vincit\template;
 
 function AlternatingBlock($data = []) {
-  $order = $data["order"] ? "reverse" : "normal";
-  $hasShortcode = $data["secondary_content"]["embed_a_shortcode"];
-  if ($hasShortcode) {
-    $secondaryContent = do_shortcode($data["secondary_content"]["shortcode"]);
-  } else {
-    $secondaryContent = $data["secondary_content"]["image"]
-      ? \Vincit\WP\Media\image($data["secondary_content"]["image"], "large", false)
-      : '<!-- no image selected -->';
-  }
+  $data = params([
+    "inverse_order" => false,
+    "content" => "",
+    "secondary_content" => [
+      "embed_a_shortcode" => false,
+      "shortcode" => null,
+      "image" => null,
+    ],
+    "background" => [
+      "image" => null,
+      "color" => null
+    ]
+  ], $data);
 
-  $background = $data["background"];
-  $bgImage = $background["image"]["url"] ?? false;
-  $bgColor = $background["color"]; ?>
-  <div class="alternating-block bg-<?=$bgColor?>" style="background-image: url('<?=$bgImage?>')">
-    <div class="container <?=$order?> <?=$hasShortcode ? "shortcode" : ""?>">
+  $bgColor = v($data, "background.color", false);
+  $bgClass = $bgColor ? "bg--$bgColor" : ""; ?>
+
+  <div
+    <?=className(
+      "alternating-block",
+      $bgClass
+    )?>
+    style="
+      background-image: url('<?=v($data, "background.image.url", false)?>')
+    "
+  >
+    <div
+      <?=className(
+        "container",
+        $data["inverse_order"] ? "reverse" : "normal",
+        v($data, "secondary_content.embed_a_shortcode") ? (
+            "shortcode"
+          ) : (
+            "no-shortcode"
+          )
+      )?>
+    >
       <div class="alternating-block__main">
         <?=$data["content"]?>
       </div>
 
       <div class="alternating-block__secondary">
-        <?=$secondaryContent?>
+        <?=v($data, "secondary_content.embed_a_shortcode") ? (
+          do_shortcode($data["secondary_content"]["shortcode"])
+        ) : (
+          \Vincit\WP\Media\image(
+            v($data, "secondary_content.image", null),
+            "large",
+            false
+          )
+        )?>
       </div>
     </div>
   </div> <?php
