070b196eaf96ba73a23bd6d03d82791693725113
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-12-20 12:03:03 +0200

    Add DOM helpers

---
+++ inc/DOM.php
@@ -0,0 +1,31 @@
+<?php
+namespace Vincit\DOM;
+
+/**
+ * DOMDocument adds "missing" doctype and head & body tags automatically
+ * so those need to be removed. $dom->saveHTML takes a node as a param, but
+ * getting a node isn't exactly straight forward, so it saves the node into a prop
+ * and that prop is then used by output() to get the right node.
+ *
+ * HTML *needs* to have a single root node, wrap multiple <li> elements in <ul> and so on.
+ *
+ * @param mixed $html
+ */
+function parse($html) {
+  $dom = new \DOMDocument();
+  $dom->loadHTML($html);
+  $dom->actualRootNode = $dom->getElementsByTagName("body")[0]->firstChild;
+
+  return $dom;
+}
+
+/**
+ * Output node.
+ *
+ * @param DOMDocument $dom
+ * @param mixed $node
+ */
+function output(\DOMDocument $dom, $node = null) {
+  $node = is_null($node) ? $dom->actualRootNode : $node;
+  return $dom->saveHTML($node);
+}
