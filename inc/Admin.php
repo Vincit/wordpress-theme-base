19beafede7beb0e4d7b2d891dcc9e1c696eb7139
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-01-02 16:36:56 +0200

    Wrap in exists check

--- inc/Admin.php
+++ inc/Admin.php
@@ -2,17 +2,19 @@
 namespace Vincit\Admin;
 
 if (is_admin()) {
-  class_alias('\GlobalMetaBoxOrder\Config', 'MetaBoxConfig');
+  if (class_exists("GlobalMetaBoxOrder\\Config")) {
+    class_alias('\GlobalMetaBoxOrder\Config', 'MetaBoxConfig');
 
-  \MetaBoxConfig::$getBlueprintUserId = function () {
-    return 2;
-  };
+    \MetaBoxConfig::$getBlueprintUserId = function () {
+      return 2;
+    };
 
-  // Admin user sets the post count and visible columns, user can't configure them
-  \MetaBoxConfig::$remove_screen_options = true;
+    // Admin user sets the post count and visible columns, user can't configure them
+    \MetaBoxConfig::$remove_screen_options = true;
 
-  // Same with metaboxes.
-  \MetaBoxConfig::$lock_meta_box_order = true;
+    // Same with metaboxes.
+    \MetaBoxConfig::$lock_meta_box_order = true;
+    }
 }
 
 // Users who will edit navigations will wonder where are the options
