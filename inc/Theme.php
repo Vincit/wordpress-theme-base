eb382a6b06604f91af2e089bffc7d2df36aea401
Nicholas Turbanov <nicholas.turbanov@vincit.fi>
2019-04-26 16:53:19 +0300

    Correct theme support argument is 'html5'

--- inc/Theme.php
+++ inc/Theme.php
@@ -11,7 +11,7 @@ add_action("after_setup_theme", function () {
   ]);
   add_theme_support("post-thumbnails");
   add_theme_support("title-tag");
-  add_theme_support("html", [
+  add_theme_support("html5", [
     "search-form",
     "comment-list",
     "gallery",
